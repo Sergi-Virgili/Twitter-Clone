f3bf1c4f81b86ed50c52d292a1f0c7101ca7be5f
Sergi-Virgili <svirgilif@gmail.com>
2023-12-02 21:51:01 +0100

    Add NextUIProvider to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Providers } from './providers'
 
 export const metadata: Metadata = {
   title: 'Clon de Twitter',
@@ -12,8 +13,12 @@ export default function RootLayout ({
   children: React.ReactNode
 }) {
   return (
-    <html lang="es">
-      <body>{children}</body>
+    <html lang="es" className='dark'>
+      <body>
+        <Providers>
+            {children}
+        </Providers>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import { redirect } from 'next/navigation'
 
 export default async function Home () {
   const supabase = createServerComponentClient({ cookies })
-  const { data: posts } = await supabase.from('posts').select('*')
   const { data: { session } } = await supabase.auth.getSession()
+
   if (session === null) {
     redirect('/login')
   }
+  const { data: posts } = await supabase
+    .from('posts')
+    .select('*, users (*)')
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AuthButtonServer />
